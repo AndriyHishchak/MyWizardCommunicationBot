10e1c2a177f0a0909264157e3b4993aef43773da
andriy hishak <you@example.com>
2020-11-07 16:56:29 +0800

    Create Wizard Bot

---
+++ src/test/java/com/home/MyWizardBot/MyWizardBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.MyWizardBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWizardBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
